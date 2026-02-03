61447ab790ec66e3ab4ce313e9f78e2abc252e64
Chris Johnson <johnsc76@unlv.neavda.edu>
2026-02-03 14:59:49 -0800

    adding some responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const searchParams = useSearchParams();
   const name = searchParams.get('name') || 'Friend';
   const [noPosition, setNoPosition] = useState({ x: 0, y: 0 });
+  const [yesClicked, setYesClicked] = useState(false);
 
   // move location of no button when attempted click
   const handleNoClick = () => {
@@ -18,14 +19,14 @@ export default function Home() {
 
   // play video on yes click
   const handleYesClick = () => {
-    
+    setYesClicked(true);
   }
 
   return (
-    <div className="flex h-screen items-center justify-center p-4">
+    <div className="flex h-screen items-center justify-center p-2 sm:p-4">
 
       {/* white box where content resides */}
-      <div className="flex flex-col bg-white h-2/3 w-1/2 rounded-lg shadow-lg items-center justify-start">
+      <div className="flex flex-col gap-y-2 sm:gap-y-6 bg-white h-[72vh] max-w-lg rounded-lg shadow-lg items-center justify-start overflow-hidden px-2 sm:px-8">
 
         {/* img */}
         <Image
@@ -33,32 +34,42 @@ export default function Home() {
           width={350}
           height={350}
           alt="Drawing of the dogs siting in front of a heart"
-          className="p-4 max-w-full h-auto"
+          className="p-2 sm:p-4 max-w-full h-auto"
         />
 
-        {/* the big question */}
-        <h1 className="font-bold text-4xl font-serif p-4 text-center line-clamp-2"> {name.toUpperCase()} WILL YOU BE MY VALENTINE? </h1>
+        {yesClicked ? (
+          // celebration video once yes is clicked
+          <video autoPlay controls className="w-full h-full rounded-lg p-4">
+            <source src="/celebration.mp4" type="video/mp4" />
+          </video>
+        ) : (
+        <>
+          {/* the big question */}
+            <h1 className="font-bold text-2xl sm:text-4xl font-serif p-4 sm:p-4 text-center break-words line-clamp-2 mb-6"> {name.toUpperCase()} WILL YOU BE MY VALENTINE? </h1> 
 
-        {/* yes & no(yes) boxes */}
-        <div className="flex flex-row w-full gap-6 justify-center items-center h-25">
-          <button 
-            onClick={handleYesClick}
-            className="bg-red-400 text-white font-semibold text-3xl rounded-full w-25 h-10 transform hover:scale-175 transition-transform"
-          >
-            YES
-          </button>
+          {/* yes & no boxes */}
+          <div className="flex flex-row w-full gap-12 justify-center items-center mt-16">
+            <button 
+              onClick={handleYesClick}
+              className="bg-red-400 text-white font-semibold text-lg sm:text-3xl rounded-full w-28 h-12 sm:w-40 sm:h-16 transform active:scale-110 hover:scale-125 transition-transform"
+            >
+              YES
+            </button>
 
-          <button
-            onClick={handleNoClick}
-            style={{
-              transform: `translate(${noPosition.x}px, ${noPosition.y}px)`,
-              transition: 'transform 0.3s ease',
-            }}
-            className="bg-gray-200 text-black font-semibold text-3xl rounded-full w-25 h-10"
-          >
-            NO
-          </button>
-        </div>
+            <button
+              onClick={handleNoClick}
+              style={{
+                transform: `translate(${noPosition.x}px, ${noPosition.y}px)`,
+                transition: 'transform 0.3s ease',
+              }}
+              className="bg-gray-200 text-black font-semibold text-lg sm:text-3xl rounded-full w-28 h-12 sm:w-40 sm:h-16"
+            >
+              NO
+            </button>
+          </div>
+        </>
+        )}
+        
 
 
       </div>
